4088358f7d8f4513e38a46ec6f19e458dd581d98
jakiboy98 <jakiboy98@gmail.com>
2015-11-18 09:24:10 -0700

    Sound/Video Project
    Play’s dragon rider and has a cat video.

---
+++ app/src/main/java/soundandvideo/controller/VideoActivity.java
@@ -0,0 +1,55 @@
+package soundandvideo.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
+import android.net.Uri;
+
+/**
+ * Created by jwil9706 on 11/10/15.
+ */
+public class VideoActivity extends Activity
+{
+    private VideoView myPlayer;
+    private Button returnButton;
+    private MediaController myVideoController;
+    private Uri videoLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        myPlayer = (VideoView) findViewById(R.id.videoView);
+        returnButton = (Button) findViewById(R.id.homeButton);
+
+        videoLocation = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cats
+        );
+        myVideoController = new MediaController(this);
+        //prepare the video
+        setupMedia();
+        setupListeners();
+    }
+
+    private void setupMedia()
+    {
+        myPlayer.setMediaController(myVideoController);
+        myPlayer.setVideoURI(videoLocation);
+    }
+
+    private void setupListeners()
+    {
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View currentView) {
+                Intent myIntent = new Intent(currentView.getContext(), SoundActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+}
